b7dcbbc4af09a9b953ec054295ab8cb853cbd27c
Kristina John (Akros AG) <kristina.john@akros.ch>
2025-08-26 09:32:05 +0200

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+                
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie!:cup_with_straw: ")
@@ -10,12 +14,12 @@ st.write(
   """
 )
 
-session = get_active_session()
+#session = get_active_session()
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
@@ -34,7 +38,7 @@ if ingredients_list:
 
     st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     st.write(my_insert_stmt)
