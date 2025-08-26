74d06c7e5db041996fd7a089429f7caf1fb0ccdd
Kristina John (Akros AG) <kristina.john@akros.ch>
2025-08-26 10:44:55 +0200

    Update streamlit_app.py
    import request to top and dataframe of json output moved to the for fruit_chosen block

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,8 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-                
+ #New section to display smoothiefroot nutrition information
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie!:cup_with_straw: ")
@@ -35,6 +34,11 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +''
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        cnx = st.connection("snowflake")
+        session = cnx.session()
 
     st.write(ingredients_string)
 
@@ -44,8 +48,4 @@ if ingredients_list:
     st.write(my_insert_stmt)
     time_to_insert = st.button("Submit Order")
 
-  #New section to display smoothiefroot nutrition information
-  import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+ 
